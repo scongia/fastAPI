bc56f9e90e894365142ededb691b081e34673503
Sergio <sergio.congia@gmail.com>
2026-05-20 13:16:32 +0000

    configurable port

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -2,10 +2,13 @@
 
 bashio::log.info "Starting UAE PDF to OFX API..."
 
+PORT=$(bashio::config 'port')
+bashio::log.info "Listening on port ${PORT}"
+
 mkdir -p /data/ofx
 
 exec uvicorn api.main:app \
     --host 0.0.0.0 \
-    --port 8099 \
+    --port "${PORT}" \
     --workers 1 \
     --log-level info
